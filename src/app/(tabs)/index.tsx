0a78e13c2289ec1ec0a40c294c77ab2fe522d431
Christopher J Baker <christopher@christopherjbaker.dev>
2026-05-22 15:22:36 +0200

    finish routing

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,14 @@
-import { Link, Stack } from "expo-router"
+import { Link } from "expo-router"
 import { StyleSheet, Text, View } from "react-native"
 
-import CurrentWeather from "../CurrentWeather"
-import Forecast from "../Forecast"
+import CurrentWeather from "../../CurrentWeather"
+import Forecast from "../../Forecast"
 
 const location = { name: "Barcelona", latitude: 41.385063, longitude: 2.173404 }
 
 const App: React.FC = () => {
   return (
     <>
-      <Stack.Screen options={{ title: "Home" }} />
-
       <View style={styles.container}>
         <Text>Weather App</Text>
 
